73d403d0c2ae7617be5632ae8406f5e5d0ddcdea
vietdesign81-lab <vietdesign81@gmail.com>
2026-06-30 01:02:43 +0700

    t
    t

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -579,7 +579,7 @@ function DesignedForSection() {
 function FloatingFooter() {
   return (
     <div className="bg-black w-full flex flex-col items-center px-[42px] py-[20px]">
-      <div className="flex flex-col md:flex-row gap-6 items-center md:items-center justify-center w-full">
+      <div className="flex flex-col md:flex-row gap-4 items-center md:items-center justify-center w-full">
         <div className="font-['Poppins:Regular',sans-serif] leading-[0] not-italic text-[#f2efec] text-center md:text-right">
   <p className="font-['Poppins:Bold',sans-serif] leading-[24px] mb-0 text-[16px]">
     We believe getting dressed should feel effortless.
